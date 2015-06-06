793dd55d998aba5ca754b591463b61434a8434ea
qianyilc <liyuelei@qianyilc.com>
2015-06-06 21:34:48 +0800

    login

--- backend/assets/AdminAsset.php
+++ backend/assets/AdminAsset.php
@@ -4,10 +4,29 @@ use sansusan\easyui\EasyuiAsset;
 
 class AdminAsset extends EasyuiAsset
 {
-
+	public static $locale = 'easyui-lang-zh_CN';
+	public static $theme = 'bootstrap';
 	public $depends = [
 		// 'yii\web\YiiAsset',
 		'yii\web\JqueryAsset',
 		'yii\bootstrap\BootstrapAsset',
 	];
+
+	protected function applyTheme()
+	{
+		if (!empty(self::$theme) && is_string(self::$theme))
+			$theme = self::$theme;
+		else
+			$theme = 'default';
+		array_unshift($this->css, strtr('themes/{theme}/easyui.css', ['{theme}' => $theme]));
+	}
+
+	protected function applyLocale()
+	{
+		if (!empty(self::$locale) && is_string(self::$locale))
+			$locale = self::$locale;
+		else
+			$locale = 'easyui-lang-ru';
+		array_push($this->js, strtr('locale/{locale}.js', ['{locale}' => $locale]));
+	}
 }
--- backend/views/index/index.php
+++ backend/views/index/index.php
@@ -17,27 +17,30 @@ $this->beginPage();
 <body>
 	<?php $this->beginBody()?>
 	<div class="container">
-		<div id="login" class="easyui-panel" title="后台登录" data-options="iconCls:'icon-application'">
-			<form id="fmLogin" method="post" novalidate>
-				<div style="margin-bottom:10px">
-					<input class="easyui-textbox" name="name"  style="width:100%;height:40px;padding:12px" data-options="prompt:'用户名',iconCls:'icon-man',iconWidth:38,validType:'length[5,15]',delay:'1000',required:true">
-				</div>
-				<div style="margin-bottom:20px">
-					<input class="easyui-textbox" type="password" name="pwd" id="pwd" style="width:100%;height:40px;padding:12px" data-options="prompt:'密码',iconCls:'icon-lock',iconWidth:38,validType:'length[5,20]',delay:'1000',required:true">
-				</div>
-				<div style="margin-bottom:20px">
-					<input class="easyui-textbox" name="verify" style="width:100%;height:40px;padding:12px" data-options="prompt:'验证码',iconCls:'icon-cup',iconWidth:38,required:true,validType:'longness[4]'">
-				</div>
-				<div style="margin-bottom:20px">
-					<img id="verifyImg1" src="{:U('Login/verify')}" align="absbottom" onClick="fleshVerify()" style="cursor:pointer;"/>
-				</div>
-				<div>
-					<a class="easyui-linkbutton" data-options="iconCls:'icon-ok'" style="padding:5px 0px;width:100%;" onclick="doLogin()">
-						<span style="font-size:14px;">登录</span>
-					</a>
-				</div>
-			</form>
+		<div class="row">
+			<div class="login" class="easyui-panel" title="后台登录" data-options="iconCls:'icon-application'">
+				<form id="fmLogin" method="post" novalidate>
+					<div style="margin-bottom:10px">
+						<input class="easyui-textbox" name="name"  style="width:100%;height:40px;padding:12px" data-options="prompt:'用户名',iconCls:'icon-man',iconWidth:38,validType:'length[5,15]',delay:'1000',required:true">
+					</div>
+					<div style="margin-bottom:20px">
+						<input class="easyui-textbox" type="password" name="pwd" id="pwd" style="width:100%;height:40px;padding:12px" data-options="prompt:'密码',iconCls:'icon-lock',iconWidth:38,validType:'length[5,20]',delay:'1000',required:true">
+					</div>
+					<div style="margin-bottom:20px">
+						<input class="easyui-textbox" name="verify" style="width:100%;height:40px;padding:12px" data-options="prompt:'验证码',iconCls:'icon-cup',iconWidth:38,required:true,validType:'longness[4]'">
+					</div>
+					<div style="margin-bottom:20px">
+						<img id="verifyImg1" src="{:U('Login/verify')}" align="absbottom" onClick="fleshVerify()" style="cursor:pointer;"/>
+					</div>
+					<div>
+						<a class="easyui-linkbutton" data-options="iconCls:'icon-ok'" style="padding:5px 0px;width:100%;" onclick="doLogin()">
+							<span style="font-size:14px;">登录</span>
+						</a>
+					</div>
+				</form>
+			</div>
 		</div>
+
 	</div>
 	<?php $this->endBody();?>
 </body>
