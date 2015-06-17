7c0c9335ae619aa22fb701f6814e0c447b77c0e8
qianyilc <liyuelei@qianyilc.com>
2015-06-17 18:26:33 +0800

    ztree

---
+++ backend/components/iMenu.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/6/17
+ * Time: 18:17
+ */
+namespace backend\components;
+use yii\widgets\Menu;
+class iMenu extends Menu
+{
+
+}
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,52 +1,15 @@
 <?php
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-</div>
+use backend\components\iMenu;
+echo iMenu::widget([
+			'items' => [
+          // Important: you need to specify url as 'controller/action',
+         // not just as 'controller' even if default action is used.
+          ['label' => 'Home', 'url' => ['site/index']],
+          // 'Products' menu item will be selected as long as the route is 'product/index'
+          ['label' => 'Products', 'url' => ['product/index'], 'items' => [
+              ['label' => 'New Arrivals', 'url' => ['product/index', 'tag' => 'new']],
+              ['label' => 'Most Popular', 'url' => ['product/index', 'tag' => 'popular']],
+          ]],
+          ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+      ],
+  ]);
